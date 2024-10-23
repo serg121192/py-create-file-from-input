b2277b3fe09615f56bceeb819f8825b91f9697d4
srx <srx92@protonmail.com>
2024-10-23 14:45:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ") + ".txt"
+    full_users_content = ""
+    while True:
+        content = input("Enter new line of content: ")
+        if content == "stop":
+            break
+        full_users_content += content + "\n"
+
+    with open(file_name, "w") as file:
+        file.write(full_users_content)
 
 
 if __name__ == "__main__":
